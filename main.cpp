72d23319ab0f3b93011ec91b35408c0e973feb73
Nikolas (Daniel) Vincenti <165183152+NikVince@users.noreply.github.com>
2025-04-17 23:28:10 +0200

    UPDATED main.cpp to implement listen and activate the previously configured socket.

--- main.cpp
+++ main.cpp
@@ -26,6 +26,8 @@ int main() {
     // binding and starting to listen == making socket available for connections
     int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
     if (rv) { die("bind()"); }
+    rv = listen(fd, SOMAXCON);
+    if (rv) { die("listen()"); }
 
     return 0;// TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
